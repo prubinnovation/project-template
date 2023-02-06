7f49daaa6142e991b95750a11e54371007fad1a1
prubinnovation <58877456+prubinnovation@users.noreply.github.com>
2023-02-06 16:21:39 +0530

    another change in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ export default function App() {
 <>
 <div className="bg-gray-100">
 <Navbar>fdgd</Navbar>
-<Section1></Section1>
+<Section1>Another change</Section1>
 <Section2></Section2>
 
 <Section3></Section3>
